e9cdb8bf77ffdc858b0c65b0e7a29c4246259897
Nick Carneiro <nickc@indeed.com>
2013-07-17 11:57:10 -0500

    extracted path to convert binary

--- gif_maker.py
+++ gif_maker.py
@@ -2,12 +2,10 @@ import os
 
 
 # returns False or filesystem path to gif
+from local import CONVERT_PATH
+
+
 def pngs_to_gif(png_dir, destination_file):
-    convert_command = 'cd ' + png_dir + ' && /usr/local/bin/convert -delay 100 -loop 0 *.png ' + destination_file
+    convert_command = 'cd ' + png_dir + ' && ' + CONVERT_PATH + ' -delay 100 -loop 0 *.png ' + destination_file
     convert_result = os.system(convert_command)
     return convert_result == 0
-
-
-if __name__ == '__main__':
-    pngs_to_gif('/Users/burt/development/gifbooth/uploads/1373942909944')
-
